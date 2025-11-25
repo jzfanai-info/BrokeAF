6f66d24fff4924ed211978ccbc00c5a10a7d8231
jzfanai <jzfanai@gmail.com>
2025-11-25 20:39:57 +0530

    message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 content: [
-  "./*.{js,ts,jsx,tsx}",
-  "./**/*.{js,ts,jsx,tsx}",
-  "./index.html"
+  "./index.html",
+  "./*.{js,ts,jsx,tsx}",     // ← ADDED
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./services/**/*.{js,ts,jsx,tsx}",
 ],
 
   darkMode: 'class',
